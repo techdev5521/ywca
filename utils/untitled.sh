adfd39971675b6300d6173f43b4ad5737db559f6
Justin Campbell <campbell.justin.tyler@gmail.com>
2017-02-06 16:45:11 -0500

    Created update utility
    Use the command ‘update-ywca’ without quotes to update the website.

---
+++ utils/untitled.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#############################################################################################
+# This script is to automatically pull changes down from GitHub and update the live website #
+#############################################################################################
+
+##########
+# Config #
+##########
+
+# Make temp folder to download into
+mkdir /var/www/html/ywca/update
+
+# Switch to newly made directory
+cd /var/www/html/ywca/update/
+
+# Pull latest file from GitHub
+wget --quiet -O git.zip https://github.com/techdev5521/ywca/archive/master.zip
+
+# Extract the file
+unzip -q git.zip
+
+# Remove existing files
+cd /var/www/html/ywca/
+rm -rf $(ls | grep -v update)
+
+# Move new files over
+cd /var/www/html/ywca/update/ywca-master/
+cp -rf $(ls | grep -v -e LICENSE -e README -e TODO -e utils) /var/www/html/ywca/
+
+# Move to web root
+cd /var/www/html/ywca
+
+# Cleanup
+rm -r update/
